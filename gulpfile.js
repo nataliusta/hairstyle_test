ab535729092b06b3756a2229ee6308ba83e515cc
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-05-19 23:56:26 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const watch = require('gulp-watch');
+//import plumber from 'gulp-plumber';
+const sass = require ('gulp-sass');
+import sourcemaps from 'gulp-sourcemaps';
+//import postcss from 'gulp-postcss';
+//import autoprefixer from 'autoprefixer';
+//import browser from 'browser-sync';
+//import csso from 'postcss-csso';
+//import rename from 'gulp-rename';
+//import htmlmin from 'gulp-htmlmin';
+//import terser from 'gulp-terser';
+//import squoosh from 'gulp-libsquoosh';
+//import svgo from 'gulp-svgmin';
+//import svgstore from 'gulp-svgstore';
+//import del from 'del';
+//import watch from 'gulp-watch';
 
 gulp.task('sass-compile', function(){
     return gulp.src('./sass/**/*.scss')
@@ -10,3 +22,30 @@ gulp.task('sass-compile', function(){
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css/'))
 })
+
+// Watcher
+
+const watcher = () => {
+    gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+    gulp.watch('source/js/*.js', gulp.series(scripts));
+    gulp.watch('source/*.html',gulp.series(html, reload));
+  }
+
+  // default
+
+  export default gulp.series(
+    clean,
+    copy,
+    copyImages,
+    gulp.parallel(
+      styles,
+      html,
+      scripts,
+      svg,
+      sprite,
+      createWebp
+    ),
+    gulp.series(
+      server,
+      watcher
+    ));
